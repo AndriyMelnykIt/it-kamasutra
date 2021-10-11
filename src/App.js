c374cc7fcfbec0d80990baf082bc0f20e6f7c561
AndriyIt <andriy523625@gmail.com>
2021-10-11 21:35:01 +0300

    save all

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ const App = (props) => {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path="/dialogs" render={() => <Dialogs
-                        state={props.state.dialogsPage}/>} />
+                        state={props.state.dialogsPage}/>}/>
 
                     <Route path="/profile" render={() => <Profile
                         profilePage={props.state.profilePage}
-                        dispatch={props.dispatch} />} />
+                        dispatch={props.dispatch}/>}/>
                 </div>
             </div>
         </BrowserRouter>
